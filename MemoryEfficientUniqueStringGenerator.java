447a60e186e333b857468e35bfc8ba43a47b9a69
MrAdi149 <singhadityakumar1911@gmail.com>
2024-11-07 10:45:32 +0530

    leet code problem solving commit

--- MemoryEfficientUniqueStringGenerator.java
+++ MemoryEfficientUniqueStringGenerator.java
@@ -4,6 +4,11 @@ import java.io.FileWriter;
 import java.util.Random;
 import java.util.Scanner;
 
+//Write a java program to accept a variable x. x can be any number from 1 to 230-1.
+//Generate a file with x number of lines and each line consist of a string that is unique with 100
+//characters long
+//Limitation: This java program needs to run with only 10M of ram
+
 public class MemoryEfficientUniqueStringGenerator {
     public static String randomString(int length){
         StringBuilder sb = new StringBuilder();
